bb1a4939cfcfe934f7bad494c571cab89e789cc0
LauraVBernal <lvanessa.bernal@udea.edu.co>
2021-09-29 11:41:23 -0500

    first commit

---
+++ ProyectoCiclo3.App.Dominio/Entidades/Estaciones.cs
@@ -0,0 +1,12 @@
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Estaciones{
+        public int id {get;set;} 
+        public string nombre {get;set;}            
+        public string direccion {get;set;}            
+        public float coord_x {get;set;}            
+        public float coord_y {get;set;}            
+        public string Tipo {get;set;}            
+           
+    } 
+} 
